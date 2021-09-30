7399a19bc8ccdbe28782fd2e33ca41f961218dc4
pavelneda <90754825+pavelneda@users.noreply.github.com>
2021-10-01 01:20:07 +0300

    Update task2_2.py

--- task2_2.py
+++ task2_2.py
@@ -1,8 +1,78 @@
+import math
 class Rational:
     def __init__(self,numerator=1,denominator=1):
-        self.__numerator=numerator
-        self.__demominator=denominator
-    def printnumbers(self):
-        return str(self.__numerator, '/' , self.__denominator)
-A=Rational(5,5)
-print(A.printnumbers())
+        newnumber=self.reducefraction(numerator,denominator)
+        self.__numerator=newnumber[0]
+        self.__denominator=newnumber[1]
+        
+    def reducefraction(self,x,y):
+        k = math.gcd(x,y)
+        return (x//k, y//k)
+      
+    def numbers(self):
+        return str(self.__numerator)+'/'+str(self.__denominator)
+        
+    def floatingnumbers(self):
+        return  float(self.__numerator)/self.__denominator
+        
+    def __add__(self,other):
+        if type(other)==type(self):
+            denominator=int(self.__denominator*other.__denominator/math.gcd(self.__denominator,other.__denominator))
+            numerator=int(denominator/self.__denominator*self.__numerator+denominator/other.__denominator*other.__numerator)
+            k = self.reducefraction(int(numerator), int(denominator))
+        else: 
+            return False
+        return Rational(k[0],k[1]) 
+    
+    def __sub__(self,other):
+        if type(other)==type(self):
+            denominator=int(self.__denominator*other.__denominator/math.gcd(self.__denominator,other.__denominator))
+            numerator=int(denominator/self.__denominator*self.__numerator-denominator/other.__denominator*other.__numerator) 
+            k=self.reducefraction(int(numerator),int(denominator))
+        else: 
+            return False
+        return Rational(k[0],k[1])
+        
+    def __mul__(self,other):
+        if type(other)==type(self):
+            denominator=self.__denominator*other.__denominator
+            numerator=self.__numerator*other.__numerator
+            k=self.reducefraction(int(numerator),int(denominator))
+        else: 
+            return False
+        return Rational(k[0],k[1])
+    
+    def __truediv__(self,other):
+        if type(other)==type(self) and other.floatingnumbers():
+            denominator=self.__denominator*other.__numerator
+            numerator=self.__numerator*other.__denominator
+            k=self.reducefraction(int(numerator),int(denominator))
+        else: 
+            return False
+        return Rational(k[0],k[1])
+        
+try:
+    firstfractione=input()
+    secondfractione=input()
+    a=list(map(int,firstfractione.split('/')))
+    b=list(map(int,secondfractione.split('/')))
+    if a[1] and len(a)==2 and b[1] and len(b)==2:
+        A=Rational(a[0],a[1])
+        B=Rational(b[0],b[1])
+    else:
+        raise
+    print("Form a/b(first): "+A.numbers())
+    print("Floatint-point format(first): "+str(A.floatingnumbers()))
+    print("Form a/b(second): "+B.numbers())
+    print("Floatint-point format(second): "+str(B.floatingnumbers()))
+    C=A+B
+    print("A+B="+C.numbers())
+    C=A*B
+    print("A*B="+C.numbers())
+    C=A-B
+    print("A-B="+C.numbers())
+    C=A/B
+    print("A/B="+C.numbers())
+
+except:
+    print("None")
